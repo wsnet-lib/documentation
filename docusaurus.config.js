a4f9efa7d9640e5684b0a3715c837bb2b13e512e
Adriano Tumminelli <adriano.tummi@hotmail.it>
2021-07-26 16:39:02 +0200

    first commit

---
+++ docusaurus.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  title: 'WSNET',
+  tagline: 'Create multiplayer games through Websockets with GameMaker Studio and Unity',
+  url: 'https://wsnet-lib.github.io',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/logo.svg',
+  organizationName: 'wsnet-lib',
+  projectName: 'documentation',
+  themeConfig: {
+    navbar: {
+      title: 'WSNET',
+      logo: {
+        alt: 'WSNET',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          href: 'https://github.com/wsnet-lib',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      copyright: `Copyright © ${new Date().getFullYear()} Adriano Tumminelli & Manuel Di Iorio`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          routeBasePath: '/',
+
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl:
+            'https://github.com/wsnet-lib/gms-client/edit/master/docs',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
